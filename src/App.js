15d8619885fc8a8c6e857d359d511f91474e58bc
unknown <sebilekayan@gmail.com>
2025-08-15 14:24:16 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import PortfolioNavbar from './components/Navbar';
-import Hero from './components/Hero';
+// import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
@@ -9,9 +9,7 @@ function App() {
   return (
     <>
       <PortfolioNavbar />
-      <Hero />
-      <Projects />
-      <Contact />
+    
     </>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default Hero

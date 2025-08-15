3f7da6e2ec15181bd923465c29de44c0d12ee989
unknown <sebilekayan@gmail.com>
2025-08-15 14:28:04 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import PortfolioNavbar from './components/Navbar';
-// import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,13 @@ function PortfolioNavbar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">My Portfolio</Navbar.Brand> */}
         <Navbar.Toggle aria-controls="navbar-nav" />
         <Navbar.Collapse id="navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            {/* <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
